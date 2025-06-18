8f9a260ed0d42d2df36d9697f5bd430ebc5aab6e
Lancelot de Ferrière <wraitii@gmail.com>
2025-06-18 18:56:37 +0200

    maintenance page

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { declareCustomElement } from 'testnet-maintenance-widget'
+declareCustomElement();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    {/* @ts-expect-error (custom element) */}
+    <maintenance-widget />
     <App />
   </StrictMode>,
 )
